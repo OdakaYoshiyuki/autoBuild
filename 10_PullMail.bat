e3ff411413c6192d88bd45231b679472079947b4
Y_ODAKA <y_odaka@hotmail.com>
2020-07-26 17:27:15 +0900

    18:00で終わるように変更

--- 10_PullMail.bat
+++ 10_PullMail.bat
@@ -13,7 +13,20 @@ git commit -a -m "フラグクリア"
 git push origin master
 
 
+rem 18:00になったら終了
+rem-----
+setlocal ENABLEDELAYEDEXPANSION
+echo !TIME!
+if "!TIME!" gtr "18:00:00.00" (
+	GOTO BAT_END
+)
+endlocal
+
+
+
 :RE_SATRT
 rem 5秒待つ
 timeout /t 5
 GOTO BAT_START
+
+:BAT_END
