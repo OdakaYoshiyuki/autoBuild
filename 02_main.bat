ecbbdc9539c53438d83044ac06801af8232596aa
Y_ODAKA <y_odaka@hotmail.com>
2020-07-26 16:19:47 +0900

    修正

--- 02_main.bat
+++ 02_main.bat
@@ -1,4 +1,4 @@
-@echo off
+
 
 git pull origin master
 
@@ -6,7 +6,7 @@ git pull origin master
 call 03_build.bat > result.txt
 cscript 04_analysis.vbs
 
+rem git commit -m "ƒrƒ‹ƒhŒ‹‰Ê‚ðpush"
 rem git push origin master
-
 
 pause
