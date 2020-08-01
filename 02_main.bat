04aead6a2ad005a4bdfc4caf6c411b6e5cb7e7e8
Y_ODAKA <y_odaka@hotmail.com>
2020-08-01 19:19:13 +0900

    ビルド時「flag.txt 宛先.txt result.txt 宛先.txt 件名.txt」のみコミット、プッシュ

--- 02_main.bat
+++ 02_main.bat
@@ -6,6 +6,8 @@ git pull origin master
 call 03_build.bat > result.txt
 cscript 04_analysis.vbs
 
-git commit -a -m "ビルド結果をpush"
+
+rem git commit -a -m "ビルド結果をpush"
+git commit -m "ビルド結果をpush" flag.txt 宛先.txt result.txt 宛先.txt 件名.txt 本文.txt
 git push origin master
 
