d9cc35c441c9f725a2662bf1e4de49eb18763029
odaka.yoshiyuki <yoshiyuki.odaka@dc.kyocera.com>
2020-08-04 08:22:55 +0900

    P4でSYNCしてからビルド

--- 02_main.bat
+++ 02_main.bat
@@ -6,9 +6,11 @@ rem setingファイルから読み込む
     SET /P PJ_NAME=
     SET /P BUILD_TYPE=
     SET /P MAIL_TYPE=
+    SET /P P4_SYNC=
 )< setting.txt
 
-
+if %P4_SYNC% == P4を最新にする p4 set %PJ_NAME%
+if %P4_SYNC% == P4を最新にする p4 sync
 
 git pull origin master
 
