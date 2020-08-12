f01785552f22cfcd42d19247714df4760ba06c46
Y_ODAKA <y_odaka@hotmail.com>
2020-08-12 15:11:11 +0900

    メール送信方法変更

--- 02_main.bat
+++ 02_main.bat
@@ -18,11 +18,12 @@ git pull origin master
 
 call 03_build.bat > result.txt
 cscript 04_analysis.vbs
+cscript 05_mail.vbs
 
 rem git commit -a -m "ビルド結果をpush"
 git commit -m "ビルド結果をpush" flag.txt 宛先.txt result.txt 件名.txt 本文.txt
 git push origin master
 
-if %MAIL_TYPE% == ビルド結果をoutlookでメール送信 cscript 20_sendOutLookMail.vbs
+rem if %MAIL_TYPE% == ビルド結果をoutlookでメール送信 cscript 20_sendOutLookMail.vbs
 
 
